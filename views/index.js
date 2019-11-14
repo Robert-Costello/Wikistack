4995931dfdedae91eed1b46301558d5760a1003f
Robert Costello <robertcostello81@gmail.com>
2019-11-14 12:59:22 -0500

    second try

---
+++ views/index.js
@@ -0,0 +1,26 @@
+const addPage = require('./addPage')
+const editPage = require('./editPage')
+const main = require('./main')
+const userList = require('./userList')
+const userPages = require('./userPages')
+const wikiPage = require('./wikipage')
+const express = require('express')
+const layout = require('./layout')
+const app = express()
+
+module.exports = { addPage, editPage, main, userList, userPages, wikiPage }
+
+app.get('/', (req, res, next) => {
+  try {
+    //await res.send(main)
+    res.send(layout())
+  } catch (error) {
+    next(error)
+  }
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`)
+})
